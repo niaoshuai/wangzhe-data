a13a6406ab8af153d555c9430048823a3909fd94
niaoshuai <niao.shuai123@163.com>
2022-06-10 14:45:27 +0800

    增加英雄详细信息

--- 技能[官方].py
+++ 技能[官方].py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     div_people_li = selector.xpath("//ul[@class='herolist clearfix']/li")
 
     # csv 写入
-    f = open('csv_英雄.csv', 'w', encoding='utf-8')
+    f = open('csv_技能.csv', 'w', encoding='utf-8')
     csv_write = csv.writer(f)
     csv_write.writerow(['英雄', '被动', '技能1', '技能2', '技能3', '技能4'])
 
---
+++ 英雄[第三方].py
@@ -0,0 +1,49 @@
+import csv
+
+from lxml import html
+
+import requests as requests
+
+
+def get_detail(name):
+    list_data = []
+    # 在下面的代码行中使用断点来调试脚本。
+    tmp_url = "https://db.18183.com" + name
+    http_response = requests.get(tmp_url)
+    response_text = http_response.text
+    selector = html.etree.HTML(response_text)
+    hero_name = selector.xpath('//div[@class="name-box"]/h1/text()')[0]
+    list_data.append(hero_name)
+    basic_type= selector.xpath('//div[@class="hero-basicinfo-box fl"]/div[@class="base"]/dl/dd/text()')[0].strip()
+    list_data.append(basic_type)
+    basic_data_li = selector.xpath('//div[@class="otherinfo-datapanel"]/ul/li')
+    for li in basic_data_li:
+        pdata_str = li.xpath("./p/text()")[0]
+        pdata = pdata_str.split("：")
+        list_data.append(pdata[1])
+    return list_data
+
+
+# 按间距中的绿色按钮以运行脚本。
+if __name__ == '__main__':
+    url = "https://db.18183.com/wzry/"
+    http_response = requests.get(url)
+    response_text = http_response.text
+    selector = html.etree.HTML(response_text)
+
+    div_people_li = selector.xpath("//ul[@class='mod-iconlist']/li")
+
+    # csv 写入
+    f = open('csv_英雄.csv', 'w', encoding='utf-8')
+    csv_write = csv.writer(f)
+    w = csv_write.writerow(["英雄名称", "英雄类型",
+                                            '最大生命', '最大法力', '物理攻击', '法术攻击', '物理防御', '物理减伤率', '法术防御', '法术减伤率', '移速',
+                                            '物理护甲穿透', '法术护甲穿透', '攻速加成', '暴击几率',
+                                            '暴击效果', '物理吸血', '法术吸血', '冷却缩减', '攻击范围', '韧性', '生命回复', '法力回复'])
+    for i in div_people_li:
+        item_href = i.xpath('./a/@href')
+        list_data = get_detail(
+            str(item_href[0]))
+        csv_write.writerow(list_data)
+
+    f.close()
